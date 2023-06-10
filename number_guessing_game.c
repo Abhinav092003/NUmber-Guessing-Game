0cc0045fb3a723c81745e5187ab88202ef908c49
Abhinav Raj <75552766+Abhinav092003@users.noreply.github.com>
2023-06-10 16:27:20 +0530

    Add files via upload

---
+++ number_guessing_game.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+    printf("Number Guessing Game BY Abhinav\n");
+    printf("play with ur friends\n");
+    printf("Who Guess In Less No Will Be Winner\n");
+
+
+ int number,guess,nguess;
+ srand(time(0));
+
+ number=rand()%100+1
+ printf("random no is %d",number);
+ 
+
+getch() ;
+} 
